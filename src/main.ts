09e295c1ef657a8df7b776cf6642c6d0136d6bfa
pashtunzoy <asifkhannoorzai@gmail.com>
2018-03-28 22:08:33 +1100

    Added comment documentation and some variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 interface IConwaysGOL {
     letsGetStarted(): string;
 }
-
+/**
+ * The main function for the Conway's Game of Life implementation
+ * 
+ * @export
+ * @class ConwaysGOL
+ * @implements {IConwaysGOL}
+ */
 export default class ConwaysGOL implements IConwaysGOL {
+    generation: number;
     letsGetStarted(): string {
         return "Woohoo";
     }
