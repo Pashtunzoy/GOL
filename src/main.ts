7552aa166548b648d66e29224c3b44d4fff48855
pashtunzoy <asifkhannoorzai@gmail.com>
2018-03-29 23:34:45 +1100

    Added a second method for restarting the game

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,15 @@ interface IConwaysGOL {
  */
 export default class ConwaysGOL implements IConwaysGOL {
     generation: number;
+    alive: number;
+
     letsGetStarted(): string {
         return "Woohoo";
+    }
+
+    restart(): void {
+        this.generation = 0;
+        this.alive = 0;
+        this.letsGetStarted();
     }
 }
